b052566b730721fb7c402b71cf1af5351bc4ffe7
Christopher Karras <ckarras11@gmail.com>
2017-12-12 20:51:12 -0500

    Created an alert router and added a get endpoint

---
+++ routes/alertRouter.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const { Alert } = require('../models/alert');
+
+const router = express.Router();
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+const jsonParser = bodyParser.json();
+
+// Get Alerts by phonenumber from DB
+router.get('/', (req, res) => {
+    Alert
+        .find()
+        .then((alerts) => {
+            res.status(200).json(alerts.map(alert => alert.apiRepr()));
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).json({ error: 'Something went wrong' });
+        });
+});
+
+// Create a new Alert
+router.post('/', (req, res) => {
+
+});
+
+// Delete an Alert
+router.delete('/', (req, res) => {
+
+});
+
+module.exports = router;
