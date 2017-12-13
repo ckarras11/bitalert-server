b33e22d637dba0634413c8c7235e18594743ab3d
Christopher Karras <ckarras11@gmail.com>
2017-12-13 12:10:41 -0500

    Added post and delete endpoints for priceRouter

--- routes/priceRouter.js
+++ routes/priceRouter.js
@@ -19,6 +19,36 @@ router.get('/', (req, res) => {
             res.status(500).json({ error: 'Something went wrong' });
         });
 });
+// Add a Price to the DB
+router.post('/', jsonParser, (req, res) => {
+    const requiredFields = ['price', 'timestamp'];
+    for (let i = 0; i < requiredFields.length; i++) {
+        const field = requiredFields[i];
+        if (!(field in req.body)) {
+            const message = `Missing ${field} in request body`;
+            console.error(message);
+            return res.status(400).send(message);
+        }
+    }
+    Price
+        .create({
+            price: req.body.price,
+            timestamp: req.body.timestamp,
+        })
+        .then(price => res.status(201).json(price.apiRepr()))
+        .catch((err) => {
+            res.status(500).json({ error: 'Something went wrong' });
+        });
+});
 
+// Remove Prices from DB
+router.delete('/:id', (req, res) => {
+    Price
+        .findByIdAndRemove(req.params.id)
+        .then(() => {
+            console.log(`Deleting alert ${req.params.id}`);
+            res.status(204).end();
+        });
+});
 
 module.exports = router;
