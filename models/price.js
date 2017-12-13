5a65866d97967721f64911318a8e02fe8cbed891
Christopher Karras <ckarras11@gmail.com>
2017-12-12 21:08:20 -0500

    Added mongoose model for price

---
+++ models/price.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const priceSchema = mongoose.Schema({
+    price: { type: Number, required: true },
+    timestamp: { type: Number, required: true },
+
+});
+
+priceSchema.methods.apiRepr = function () {
+    return {
+        id: this.id,
+        price: this.price,
+        timestamp: this.timestamp,
+    };
+};
+
+const Price = mongoose.model('Price', priceSchema, 'Price');
+
+module.exports = { Price };
